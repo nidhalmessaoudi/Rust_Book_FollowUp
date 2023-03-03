ea0f96ad67546057c036b5b1bfe32185f7e67462
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2023-03-03 20:31:37 +0100

    Variables in Rust

---
+++ variables/src/main.rs
@@ -0,0 +1,23 @@
+const TWO_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    println!("The number of seconds in 2 hours is: {TWO_HOURS_IN_SECONDS}");
+
+    let y = 8;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+
+    println!("The value of y is: {y}");
+
+    let spaces = "       ";
+    let spaces = spaces.len();
+    println!("The number of spaces is: {spaces}");
+}
