ce7f1cb1944ab0d1d75c80052eefcf213af44c2e
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2023-03-17 12:40:16 +0100

    Working with enums

---
+++ enums/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
+
+    // let localhost = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1")
+    // };
+
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1")
+    // };
+
+    let localhost = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String)
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn call(&self) {
+    }
+}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String
+// }
